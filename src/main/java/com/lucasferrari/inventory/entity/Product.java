3834fac67eb2f497a98546faf61d0619bf364dc5
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2026-02-05 11:43:33 -0300

    feat: create Product entity

---
+++ src/main/java/com/lucasferrari/inventory/entity/Product.java
@@ -0,0 +1,25 @@
+package com.lucasferrari.inventory.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+}
